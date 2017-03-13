f0d3c60addafa036b82d72619bf2cfed52047edf
Brandon Vicinus <brandonvicinus@gmail.com>
2017-03-13 11:31:21 -0400

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const App = ()  => <h1>Hi </h1>
+class App extends React.Component {
+    render(){
+        return (
+         <div>
+            <h1>Hello Series</h1> 
+             <b>Bold Text</b>
+         </div>
+        )
+    }
+}
+
+//const App = () => <h1>Hello stateless </h1>
 
 export default App
